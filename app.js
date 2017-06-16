3f7b65e07c7747bdfdc4f68f28a44f13c42d5b2e
Austin DeGarmo <austindegarmo@yahoo.com>
2017-06-15 23:48:58 -0700

    second push

--- app.js
+++ app.js
@@ -1,20 +1,78 @@
 //create needed global variables
 
 var wins = 0;
-
 var losses = 0;
-
 var guessesLeft = 10;
-
 var wrongGuesses = [];
-
+var computerChoice = "";
+var userChoice = "";
 
 //the computre picks a letter, the user picks a letter of the alphabet. 
 
-var alphabet = ['a','b','c','d','e','f']
-var randomIndex = Math.floor(Math.random() * alphabet.length);
-console.log('randomIndex' , randomIndex);
+function computerMakesChoice() {
 
+	var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+	var randomIndex = alphabet[Math.floor(Math.random() * alphabet.length)];
+	console.log('randomIndex' , randomIndex);
+
+
+	computerChoice = [randomIndex];
+	console.log('computerChoice', computerChoice);
+	return computerChoice;
+}
+
+
+
+computerMakesChoice();
+
+
+	document.onkeyup = function(event) {
+		console.log('event', event);
+		userChoice = event.key;
+		console.log('userChoice1', userChoice);
+		
+	
+
+
+
+
+	// console.log('userChoice2', userChoice)
+
+	if (userChoice === computerChoice) {
+		wins++;
+		// console.log()
+		var victories = document.getElementById("dubs");
+		victories.innerHTML = wins;
+	}
+	else if (guessesLeft === 0) {
+		losses++;
+		// console.log()
+		var shortcomings = document.getElementById("failures");
+			shortcomings.innerHTML = losses;
+			 guessesLeft = 10;
+
+	}
+	else {
+		guessesLeft--;
+		var attempts = document.getElementById("attempts");
+			attempts.innerHTML = guessesLeft;
+	}
+}
+
+
+
+	
+
+
+
+
+
+	// var html = "<p> Win counter </p>" + "<p>wins: " + wins + "</p>" + "<p>losses: " + losses + "</p>";
+
+	
+
+
+//Check win condition
 //Then we need to compare the two.
 
 
